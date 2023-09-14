65d6e3803a983f2a15ce8e0e9eb3adf04dd408d4
mneedham01 <mneedham@smith.edu>
2023-09-14 12:32:19 -0400

    added test for newGreeting()

--- app/src/test/java/practice/AppTest.java
+++ app/src/test/java/practice/AppTest.java
@@ -11,4 +11,9 @@ public class AppTest {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
+
+    @Test public void greetingCorrect() {
+        App classUnderTest = new App();
+        assertEquals("Hi Maggie!", classUnderTest.newGreeting());
+    }
 }
